8c2a7813be3389146cf105b1b83c49f7dc12cba8
anishbnair <nbanish@gmail.com>
2018-06-09 18:37:24 -0400

    updated index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import thunk from 'redux-thunk';
+
+import { reducer } from './reducers';
+
+const storeEnhancers = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension && window.devToolsExtension()
 );
+
+
+const store = createStore(
+  reducer,
+  {
+    search: '',
+    query: '',
+    show_favorites: false,
+    results: [],
+    favorites: [],
+    current_page: 1,
+    offset: 0
+  } ,
+  storeEnhancers);
+
+
+ReactDOM.render((
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
+), document.getElementById('root'));
 registerServiceWorker();
